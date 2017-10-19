36d4d05f84c8c0fdb4e8cc3c562d5f403a64989f
Jay Turner <trainerdex@jayturnr.me>
2017-10-20 00:33:37 +0100

    Another typo

--- useful/useful.py
+++ useful/useful.py
@@ -26,7 +26,7 @@ class Useful:
 	@commands.command()
 	async def migration(self):
 		migrations = rrule(WEEKLY, dtstart=datetime(2017,2,23,0,0), interval=2, byweekday=(TH,))
-		await self.bot.say("The next migration is in {}".format(humanize.naturaltime([x for x in fortnight if x >= datetime.now()][0])))
+		await self.bot.say("The next migration is in {}".format(humanize.naturaltime([x for x in migrations if x >= datetime.now()][0])))
 
 def setup(bot):
 	bot.add_cog(Useful(bot))
